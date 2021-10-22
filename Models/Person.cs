ed103908bae0a2fe613b07bf0b0d77700f85dad0
RafaelNovo24 <rafaelnovo24@gmail.com>
2021-10-22 09:49:12 +0100

    Commit

---
+++ Models/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingLinq.Models
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return (DateTime.Now.Year - Birthday.Year);
+            }
+        }
+
+        public Person()
+        {
+
+        }
+
+        public static List<Person> PersonList()
+        {
+            List<Person> person = new List<Person>();
+
+            person.Add(new Person { FirstName = "Tim", LastName = "Corey", Birthday = Convert.ToDateTime("25/02/1970")});
+            person.Add(new Person { FirstName = "Joe", LastName = "Smith", Birthday = Convert.ToDateTime("31/03/1980")});
+            person.Add(new Person { FirstName = "Sue", LastName = "Storm", Birthday = Convert.ToDateTime("03/01/1964")});
+            person.Add(new Person { FirstName = "Sara", LastName = "Jones", Birthday = Convert.ToDateTime("06/03/1996")});
+            person.Add(new Person { FirstName = "Jamie", LastName = "Doe", Birthday = Convert.ToDateTime("18/02/2001")});
+            person.Add(new Person { FirstName = "Mary", LastName = "Smith", Birthday = Convert.ToDateTime("23/01/1988")});
+
+            return person;
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using UsingLinq.Models;
+using static System.Console;
+
+namespace UsingLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.PersonList();
+
+            // x equals person. It just doesn't repeat itself like the foreach. However x can be any other value ex:. "K" , "value" , "Nothing" 
+            //Order by FirstName
+            person = person.OrderBy(x=> x.FirstName).ToList();
+
+            WriteLine("Order by FirstName");
+            foreach (var p in person)
+            {
+                WriteLine($"{p.FirstName} | {p.LastName} | {p.FullName} | {p.Age}");
+            }
+
+            WriteLine("*************************************");
+
+            //Order by FirstName and then by age desc
+            person = person.OrderBy(x => x.FirstName).ThenByDescending(x=>x.Age).ToList();
+
+            WriteLine("Order by FirstName and then by age desc");
+            foreach (var p in person)
+            {
+                WriteLine($"{p.FirstName} | {p.LastName} | {p.FullName} | {p.Age}");
+            }
+
+            WriteLine("*************************************");
+
+            //Filter list to a smaller group where age above 25 yo
+            person = person.Where(y => y.Age > 25).ToList();
+
+            WriteLine("Filter list to a smaller group where age above 25 yo");
+            foreach (var p in person)
+            {
+                WriteLine($"{p.FirstName} | {p.LastName} | {p.FullName} | {p.Age}");
+            }
+
+            WriteLine("*************************************");
+
+            //Filter list to a smaller group where age above 25 yo and birthday month == 3
+            person = person.Where(y => y.Age > 25 && y.Birthday.Month == 03).ToList();
+
+            WriteLine("Filter list to a smaller group where age above 25 yo and birthday month == 3");
+            foreach (var p in person)
+            {
+                WriteLine($"{p.FirstName} | {p.LastName} | {p.FullName} | {p.Age}");
+            }
+
+            WriteLine("*************************************");
+
+            //Sum the age of all the people in the list
+            int totalAge = person.Sum(k => k.Age);
+
+            WriteLine("Sum the age of all the people in the list");
+            WriteLine($"The sum of the age is: {totalAge}");
+
+            WriteLine("*************************************");
+
+            //Sum the age of the people where age is above 20
+            int ageOfSome = person.Where(k => k.Age > 20).Sum(k => k.Age);
+
+            WriteLine("Sum the age of the people where age is above 20");
+            WriteLine($"The sum of the age is: {ageOfSome}");
+
+            WriteLine("*************************************");
+
+            //Average age
+            double averageAge = person.Average(k => k.Age);
+
+            WriteLine("Average age.");
+            WriteLine($"The average age is: {averageAge}");
+
+            WriteLine("*************************************");
+
+            //Complex query using from -- Must use group or select at the end
+            var names = from not in person where not.Age > 20 && not.Age < 50 select not.FullName;
+
+            WriteLine($"Complex query using from -- Must use group or select at the end");
+            foreach (var item in names)
+            {
+                WriteLine(item);
+            }
+
+            WriteLine("*************************************");
+
+            //Complex query using from , ordering by the new key and group by lastname
+            var complexQuery = from not in person where not.Age > 20 && not.Age < 70 group not by not.FullName into newList orderby newList.Key select newList.Key;
+
+            WriteLine($"Complex query using from , ordering by the new key and group by lastname");
+            foreach (var item in complexQuery)
+            {
+                WriteLine(item);
+            }
+
+            WriteLine("*************************************");
+
+            WriteLine("Recommended website: https://www.tutorialsteacher.com/linq/sample-linq-queries");
+
+            ReadLine();
+        }
+    }
+}
